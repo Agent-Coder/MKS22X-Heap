6565cc93bb19b9458ad5ffeec691eca62c234858
Amanda Zheng <azheng00@stuy.edu>
2019-04-21 17:40:58 -0400

    finished heapify

--- MyHeap.java
+++ MyHeap.java
@@ -25,10 +25,13 @@
        swap(data,index,index-1)/2);
      }
    }
-   public static void heapify(int[]){
-
+   public static void heapify(int[] data){
+     for(int i=data.length-1,i>=0;i--){
+       swap(data,i,0);
+       pushDown(data, data.length, 0);
+     }
    }
-   public static void heapsort(int[]){
+   public static void heapsort(int[] data){
      //need to finish fitrst
    }
 
